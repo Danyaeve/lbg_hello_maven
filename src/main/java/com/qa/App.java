8c896bed9b48c42f95b5d2cc71569b113b9f6a63
Danya <danyaevehemingway@hotmail.co.uk>
2025-12-03 11:45:38 +0000

    goodbye message updated

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -22,7 +22,7 @@ public class App
     }
 
     public static String sayGoodbye(){
-        return "Goodbye World....!";
+        return "Goodbye World!";
     }
 
     public static String sayHelloToSomeone(String name){
